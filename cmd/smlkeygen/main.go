e92947d5ce3676605e1a907c703bc45096ebd677
He Liu <liulonnie@gmail.com>
2017-10-18 18:42:30 -0700

    .

---
+++ cmd/smlkeygen/main.go
@@ -0,0 +1,40 @@
+package smlkeygen
+
+import (
+	"flag"
+	"io/ioutil"
+	"log"
+
+	"shanhu.io/aries/creds"
+)
+
+func ne(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func main() {
+	out := flag.String("out", "", "key path to output")
+	nopass := flag.Bool("nopass", false, "no passphrase")
+	flag.Parse()
+
+	var pwd []byte
+	var err error
+
+	if !*nopass {
+		pwd, err = creds.ReadPassword("Key passphrase: ")
+		ne(err)
+	}
+
+	pri, pub, err := creds.GenerateKey(pwd)
+	ne(err)
+
+	if *out == "" {
+		*out, err = creds.HomeFile("key")
+		ne(err)
+	}
+
+	ne(ioutil.WriteFile(*out+".pem", pri, 0600))
+	ne(ioutil.WriteFile(*out+".pub", pub, 0600))
+}
