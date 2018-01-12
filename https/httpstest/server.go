5659832ba4165b9893f8b2f82f978f36e4ff5d5a
Lonnie Liu <liulonnie@gmail.com>
2018-01-11 21:49:57 -0800

    server test

--- https/httpstest/server.go
+++ https/httpstest/server.go
@@ -9,7 +9,6 @@ import (
 type Server struct {
 	*httptest.Server
 
-	Host      string // test host
 	Transport *http.Transport
 }
 
@@ -19,7 +18,7 @@ func (s *Server) Client() *http.Client {
 	return &http.Client{Transport: s.Transport}
 }
 
-// NewServer creates an HTTPS server at the given testing domains.
+// NewServer creates an HTTPS server for the given testing domains.
 func NewServer(domains []string, h http.Handler) (*Server, error) {
 	c, err := NewTLSConfigs(domains)
 	if err != nil {
@@ -32,8 +31,43 @@ func NewServer(domains []string, h http.Handler) (*Server, error) {
 
 	serverHost := server.Listener.Addr().String()
 	return &Server{
-		Host:      serverHost,
 		Server:    server,
 		Transport: c.Sink(serverHost),
 	}, nil
+}
+
+// DualServer wraps two *httptest.Server's with a transport that
+// goes to one of them base on HTTP or HTTPS.
+type DualServer struct {
+	HTTP      *httptest.Server
+	HTTPS     *httptest.Server
+	Transport *http.Transport
+}
+
+// NewDualServer creates an HTTPS dual server for the given testing domains.
+func NewDualServer(domains []string, h http.Handler) (*DualServer, error) {
+	c, err := NewTLSConfigs(domains)
+	if err != nil {
+		return nil, err
+	}
+
+	httpsServer := httptest.NewUnstartedServer(h)
+	httpsServer.TLS = c.Server
+	httpsServer.StartTLS()
+
+	httpServer := httptest.NewServer(h)
+
+	httpAddr := httpServer.Listener.Addr().String()
+	httpsAddr := httpsServer.Listener.Addr().String()
+	return &DualServer{
+		HTTP:      httpServer,
+		HTTPS:     httpsServer,
+		Transport: c.SinkHTTPS(httpAddr, httpsAddr),
+	}, nil
+}
+
+// Client creates an HTTP client which transport connects directly to one
+// of the servers base on the protocol port.
+func (s *DualServer) Client() *http.Client {
+	return &http.Client{Transport: s.Transport}
 }
--- https/httpstest/server_test.go
+++ https/httpstest/server_test.go
@@ -8,6 +8,17 @@ import (
 	"net/http"
 )
 
+func checkBody(t *testing.T, resp *http.Response, msg string) {
+	bs, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		t.Fatalf("read body: %s", err)
+	}
+	got := string(bs)
+	if got != msg {
+		t.Errorf("response body want: %q, got %q", msg, got)
+	}
+}
+
 func TestServer(t *testing.T) {
 	const msg = "hello"
 	s, err := NewServer(
@@ -27,12 +38,36 @@ func TestServer(t *testing.T) {
 	}
 	defer resp.Body.Close()
 
-	bs, err := ioutil.ReadAll(resp.Body)
+	checkBody(t, resp, msg)
+}
+
+func TestDualServer(t *testing.T) {
+	const msg = "hello"
+
+	s, err := NewDualServer(
+		[]string{"test.shanhu.io"},
+		http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+			fmt.Fprint(w, msg)
+		}),
+	)
 	if err != nil {
-		t.Fatalf("read body: %s", err)
+		t.Fatalf("create server: %s", err)
 	}
-	got := string(bs)
-	if got != msg {
-		t.Errorf("response body want: %q, got %q", msg, got)
+
+	c := s.Client()
+	resp1, err := c.Get("https://test.shanhu.io")
+	if err != nil {
+		t.Fatalf("get: %s", err)
 	}
+	defer resp1.Body.Close()
+
+	checkBody(t, resp1, msg)
+
+	resp2, err := c.Get("http://test.shanhu.io")
+	if err != nil {
+		t.Fatalf("get: %s", err)
+	}
+	defer resp2.Body.Close()
+
+	checkBody(t, resp2, msg)
 }
