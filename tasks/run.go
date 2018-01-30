89d3e9ddeafb3bfbbe3127b9b68ee0704a494277
He Liu <liulonnie@gmail.com>
2018-01-29 20:40:29 -0800

    task running system

---
+++ tasks/run.go
@@ -0,0 +1,40 @@
+package tasks
+
+import (
+	"fmt"
+	"log"
+	"path"
+
+	"shanhu.io/aries/creds"
+)
+
+// Run issues a list of tasks to a particular server.
+func Run(server, prefix string, tasks []string) error {
+	c, err := creds.Dial(server)
+	if err != nil {
+		return err
+	}
+
+	if len(tasks) == 0 {
+		// list available tasks
+		var names []string
+		p := path.Join(prefix, "help")
+		if err := c.JSONCall(p, nil, &names); err != nil {
+			return err
+		}
+
+		for _, name := range names {
+			fmt.Println(name)
+		}
+		return nil
+	}
+
+	for _, t := range tasks {
+		log.Println(t)
+		if err := c.Poke(path.Join(prefix, t)); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
--- tasks/tasks.go
+++ tasks/tasks.go
@@ -1,18 +1,19 @@
-package aries
+package tasks
 
 import (
 	"path"
 	"sort"
 
+	"shanhu.io/aries"
 	"shanhu.io/misc/errcode"
 )
 
 type tasks struct {
 	lst []string
-	m   map[string]Func
+	m   map[string]aries.Func
 }
 
-func newTasks(m map[string]Func) *tasks {
+func newTasks(m map[string]aries.Func) *tasks {
 	var lst []string
 	for name := range m {
 		lst = append(lst, name)
@@ -24,20 +25,20 @@ func newTasks(m map[string]Func) *tasks {
 	}
 }
 
-func (t *tasks) serve(c *C) error {
+func (t *tasks) serve(c *aries.C) error {
 	name := path.Base(c.Path)
 	f, found := t.m[name]
 	if !found {
 		if name == "help" {
-			return ReplyJSON(c, t.lst)
+			return aries.ReplyJSON(c, t.lst)
 		}
 		return errcode.InvalidArgf("unknown task: %q", name)
 	}
 	return f(c)
 }
 
-// ServeTask returns the serving function for a task list.
-func ServeTask(tasks map[string]Func) Func {
+// Serve returns the serving function for a task list.
+func Serve(tasks map[string]aries.Func) aries.Func {
 	t := newTasks(tasks)
 	return t.serve
 }
