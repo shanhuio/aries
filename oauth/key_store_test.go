cdb60b080798c766498209307f848b696d70d111
He Liu <liulonnie@gmail.com>
2018-03-15 22:16:07 -0700

    key store test

--- oauth/key_store_test.go
+++ oauth/key_store_test.go
@@ -28,3 +28,26 @@ func TestMemKeyStore(t *testing.T) {
 		t.Errorf("2nd time, want %q, got %q", string(k), string(got))
 	}
 }
+
+func TestFileKeyStore(t *testing.T) {
+	s := NewFileKeyStore(map[string]string{
+		"h8liu": "testdata/h8liu.key",
+		"yumuzi": "testdata/yumuzi.key",
+	})
+
+	for _, test := range []struct {
+		user, key string
+	} {
+		{"h8liu", "h8\n"},
+		{"yumuzi", "work?\n"},
+		{"xuduoduo", ""},
+	} {
+		got, err := s.Key(test.user)
+		if err != nil {
+			t.Fatal(err)
+		}
+		if string(got) != test.key {
+			t.Errorf("want %q, got %q", test.key, string(got))
+		}
+	}
+}
