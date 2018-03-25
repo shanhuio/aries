c1965b096523ce014d3eaf106021ac8a1bf42b78
He Liu <liulonnie@gmail.com>
2018-03-25 12:51:56 -0700

    static server test

---
+++ static/main_test.go
@@ -0,0 +1,36 @@
+package static
+
+import (
+	"testing"
+
+	"shanhu.io/aries"
+	"shanhu.io/aries/ariestest"
+	"smallrepo.com/base/httputil"
+)
+
+func TestMain(t *testing.T) {
+	config := &config{Dir: "testdata"}
+	logger := ariestest.NewLogger(t)
+	service, err := main(&aries.Env{Config: config, Logger:logger})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	s, err := ariestest.HTTPSServer("shanhu.io", service)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer s.Close()
+
+	c := httputil.NewClient("https://shanhu.io")
+	c.Transport = s.Transport
+
+	str, err := c.GetString("/")
+	if err != nil {
+		t.Fatal(err)
+	}
+	const want = "hello\n"
+	if str != want {
+		t.Errorf("get /, want %q, got %q", want, str)
+	}
+}
