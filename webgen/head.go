24fb1fac15fea466aba84ed18cf68a6d389804a9
He Lonnie Liu <liulonnie@gmail.com>
2019-01-20 22:54:26 -0800

    .

--- webgen/head.go
+++ webgen/head.go
@@ -1,6 +1,15 @@
 package webgen
 
-// NewMeta create a new meta tag.
+// NewMeta creates a new meta tag.
 func NewMeta(key, value string) *Node {
 	return Meta(Attrs{key: value})
+}
+
+// NewCSSLink creates a new CSS link ini uh 6tg6fdb n  bnnnnnnnnnnn<F6><F6>
+func NewCSSLink(href string) *Node {
+    return Link(Attrs{
+        "rel": "stylesheet",
+        "type": "text/css",
+        "href": href,
+    })
 }
