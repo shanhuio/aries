450cf6f3487b23065ef212d57dff6961c78b3f70
Leon <leonreed@users.noreply.github.com>
2019-06-10 07:44:25 -0700

    single admin

---
+++ oauth/single_admin.go
@@ -0,0 +1,11 @@
+package oauth
+
+// SingleAdmin returns a user check function for a single admin.
+func SingleAdmin(admin string) func(user string) (interface{}, int, error) {
+	return func(user string) (interface{}, int, error) {
+		if user == admin {
+			return user, 10, nil
+		}
+		return "", 0, nil
+	}
+}
