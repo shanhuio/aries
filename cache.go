e931f8029e32af7afede233e52ac3ad9073a8bc9
Lonnie Liu <liulonnie@gmail.com>
2020-12-21 19:55:13 -0800

    fix comment on NeverCache

--- cache.go
+++ cache.go
@@ -1,6 +1,6 @@
 package aries
 
-// NeverCache sets the Cache-Control header to "no-store".
+// NeverCache sets the Cache-Control header to "max-age=0; no-store".
 func NeverCache(c *C) {
 	c.Resp.Header().Set("Cache-Control", "max-age=0; no-store")
 }
